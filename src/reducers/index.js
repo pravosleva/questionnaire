dfa3c00906f5fada9ecb5943c2d6bead94b5cd87
pravosleva <selection4test@gmail.com>
2017-08-11 15:24:48 +0300

    Updates

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,7 +6,7 @@ export default (
       {
         "formName": "Чиллер",
         "inputForms": [
-          {"inputType": "input", "label": "Номер задачи", "required": true, "value":""},
+          { "inputType": "input", "label": "Номер задачи", "required": true, "value":"" },
           {
             "inputType": "dropdown",
             "label": "Укажите тип чиллера",
@@ -20,14 +20,8 @@ export default (
                 "typeName": "AIR-WATER",
                 "liquid1Typelist": [
                   { "liquidType": "WATER" },
-                  {
-                    "liquidType": "MEG",
-                    "percentage": ""
-                  },
-                  {
-                    "liquidType": "MPG",
-                    "percentage": ""
-                  }
+                  { "liquidType": "MEG", "percentage": "" },
+                  { "liquidType": "MPG", "percentage": "" }
                 ],
                 "pumpStationTypelist": [
                   { "typeName": "1 PUMP" },
@@ -40,73 +34,95 @@ export default (
                 "typeName": "WATER-WATER",
                 "liquid1Typelist": [
                   { "liquidType": "WATER" },
-                  {
-                    "liquidType": "MEG",
-                    "percentage": ""
-                  },
-                  {
-                    "liquidType": "MPG",
-                    "percentage": ""
-                  }
+                  { "liquidType": "MEG", "percentage": "" },
+                  { "liquidType": "MPG", "percentage": "" }
                 ],
                 "liquid2Typelist": [
                   { "liquidType": "WATER" },
-                  {
-                    "liquidType": "MEG",
-                    "percentage": ""
-                  },
-                  {
-                    "liquidType": "MPG",
-                    "percentage": ""
-                  }
+                  { "liquidType": "MEG", "percentage": "" },
+                  { "liquidType": "MPG", "percentage": "" }
                 ]
               },
               {
                 "typeName": "CONDENSERLESS",
                 "liquid1Typelist": [
                   { "liquidType": "WATER" },
-                  {
-                    "liquidType": "MEG",
-                    "percentage": ""
-                  },
-                  {
-                    "liquidType": "MPG",
-                    "percentage": ""
-                  }
+                  { "liquidType": "MEG", "percentage": "" },
+                  { "liquidType": "MPG", "percentage": "" }
                 ]
               }
             ]
           },
-          {"inputType": "input", "label": "Требуемая холодопроизводительность", "required": true, "value":""},
-          {"inputType": "input", "label": "Бренд", "required": false, "value":""}
+          { "inputType": "input", "label": "Требуемая холодопроизводительность", "required": true, "value":"" },
+          { "inputType": "input", "label": "t воды на входе, C", "required": true, "value":"" },
+          { "inputType": "input", "label": "t воды на выходе, C", "required": true, "value":"" },
+          { "inputType": "input", "label": "Бренд", "required": false, "value":"" }
         ]
       },
       {
         "formName": "Драйкуллер",
         "inputForms": [
-          {"inputType": "input", "label": "Номер задачи", "required": true, "value":""},
-          {"inputType": "input", "label": "Требуемая холодопроизводительность", "required": true, "value":""},
-          {"inputType": "input", "label": "Дополнительные требования", "required": false, "value":""},
-          {"inputType": "input", "label": "Бренд", "required": false, "value":""}
+          { "inputType": "input", "label": "Номер задачи", "required": true, "value":"" },
+          {
+            "inputType": "dropdown",
+            "label": "Укажите тип жидкости",
+            "required": true,
+            "value": "",
+            "liquid1TypeValue": "",
+            "pumpStationTypeValue": "",
+            "equipmentTypelist": [
+              {
+                "typeName": "SELECTED",
+                "liquid1Typelist": [
+                  { "liquidType": "WATER" },
+                  { "liquidType": "MEG", "percentage": "" },
+                  { "liquidType": "MPG", "percentage": "" }
+                ]
+              },
+            ]
+          },
+          { "inputType": "input", "label": "Требуемая холодопроизводительность", "required": true, "value":"" },
+          { "inputType": "input", "label": "t воды на входе, C", "required": true, "value":"" },
+          { "inputType": "input", "label": "t воды на выходе, C", "required": true, "value":"" },
+          { "inputType": "input", "label": "Дополнительные требования", "required": false, "value":"" },
+          { "inputType": "input", "label": "Бренд", "required": false, "value":"" }
         ]
       },
       {
         "formName": "Элементы вентиляции / Центральный кондиционер",
         "inputForms": [
-          {"inputType": "input", "label": "Номер задачи", "required": true, "value":""},
-          {"inputType": "input", "label": "Расход воздуха", "required": true, "value":""},
-          {"inputType": "input", "label": "Сопротивление сети воздуховодов", "required": true, "value":""},
-          {"inputType": "input", "label": "Бренд", "required": false, "value":""}
+          { "inputType": "input", "label": "Номер задачи", "required": true, "value":"" },
+          { "inputType": "input", "label": "Расход воздуха", "required": true, "value":"" },
+          { "inputType": "input", "label": "Сопротивление сети воздуховодов", "required": true, "value":"" },
+          { "inputType": "input", "label": "Бренд", "required": false, "value":"" }
         ]
       },
       {
         "formName": "Градирня",
         "inputForms": [
-          {"inputType": "input", "label": "Номер задачи", "required": true, "value":""},
-          {"inputType": "input", "label": "Требуемая холодопроизводительность", "required": true, "value":""},
-          {"inputType": "input", "label": "t мокрого термометра", "required": true, "value":""},
-          {"inputType": "input", "label": "Регион", "required": true, "value":""},
-          {"inputType": "input", "label": "Бренд", "required": false, "value":""}
+          { "inputType": "input", "label": "Номер задачи", "required": true, "value":"" },
+          {
+            "inputType": "dropdown",
+            "label": "Укажите тип жидкости",
+            "required": true,
+            "value": "",
+            "liquid1TypeValue": "",
+            "pumpStationTypeValue": "",
+            "equipmentTypelist": [
+              {
+                "typeName": "SELECTED",
+                "liquid1Typelist": [
+                  { "liquidType": "WATER" },
+                  { "liquidType": "MEG", "percentage": "" },
+                  { "liquidType": "MPG", "percentage": "" }
+                ]
+              },
+            ]
+          },
+          { "inputType": "input", "label": "Требуемая холодопроизводительность", "required": true, "value":"" },
+          { "inputType": "input", "label": "t мокрого термометра", "required": true, "value":"" },
+          { "inputType": "input", "label": "Регион", "required": true, "value":"" },
+          { "inputType": "input", "label": "Бренд", "required": false, "value":"" }
         ]
       }
     ]
